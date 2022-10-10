9764aade9407aa6bed52d0a06f1f0368a81dac03
Annge001 <anngela0222@gmail.com>
2022-10-10 07:36:09 -0300

    up

--- src/app/components/editar-alumno-form/editar-alumno-form.component.ts
+++ src/app/components/editar-alumno-form/editar-alumno-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Output, EventEmitter, OnInit} from '@angular/core';
+import {FormArray, FormGroup, Validators, FormControl, FormBuilder} from "@angular/forms";
+import {ReactiveFormsModule} from '@angular/forms';
+import {outputAst} from "@angular/compiler";
+
 
 @Component({
   selector: 'app-editar-alumno-form',
@@ -8,10 +12,14 @@ import { Component, OnInit } from '@angular/core';
 export class EditarAlumnoFormComponent implements OnInit {
 
 
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
   constructor() { }
 
+
+
   ngOnInit(): void {
   }
+
 
 }
