71ae8db2d87a7dd7594cd8f9c800f48d81bc6a2b
Annge001 <anngela0222@gmail.com>
2022-10-23 16:43:48 -0300

    cree servicio validator-login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  formularioPersona: FormGroup;
+  isEmailValid: boolean = true;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    public validator: ValidatorService
+  ) {
+    this.formularioPersona = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
+    console.log(this.formularioPersona);
+
   }
+
+  loginUsuario(){
+    this.validarEmail();
+    this.formularioPersona.reset();
+
+  }
+  validarEmail(){
+    // @ts-ignore
+    this.isEmailValid = this.validator.esEmailValido(this.formularioPersona.get('email').value);
+    console.log(this.formularioPersona)
+  }
+
 
 }
