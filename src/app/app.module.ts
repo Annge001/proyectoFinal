9ab7a42caca5dda1b1b62ba97a8d49a9b439d943
Annge001 <anngela0222@gmail.com>
2022-10-24 21:09:07 -0300

    mas modulos, todos importados y declarados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,28 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {ListaComponent} from "./modulos/alumnos/components/lista/lista.component";
-import {HeaderComponent} from "./components/header/header.component";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material.module";
 import {LateralNavbarComponent} from "./components/lateral-navbar/lateral-navbar.component";
-import {EditarAlumnoFormComponent} from "./modulos/alumnos/components/editar-alumno-form/editar-alumno-form.component";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {FormsModule} from "@angular/forms";
 import {PrincipalComponent} from "./components/principal/principal.component";
 import {MostrarApellidoPipe} from "./mostrar-apellido.pipe";
 import {TamanoLetraDirective} from "./directivas/tamano-letra.directive";
-import {AltaAlumnoComponent} from "./modulos/alumnos/components/alta-alumno/alta-alumno.component";
 import {CursoService} from "./modulos/cursos/services/curso.service";
 import {AlumnosService} from "./modulos/alumnos/services/alumnos.service";
 import {CursosModule} from "./modulos/cursos/cursos.module";
 import {AutenticacionModule} from "./modulos/autenticacion/autenticacion.module";
+import {CoreModule} from "./core/core.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaComponent,
-    HeaderComponent,
     LateralNavbarComponent,
-    EditarAlumnoFormComponent,
     PrincipalComponent,
     MostrarApellidoPipe,
     TamanoLetraDirective,
-    AltaAlumnoComponent,
 
   ],
   imports: [
@@ -43,6 +36,7 @@ import {AutenticacionModule} from "./modulos/autenticacion/autenticacion.module"
     FormsModule,
     CursosModule,
     AutenticacionModule,
+    CoreModule
 
   ],
   providers: [CursoService, AlumnosService],
