7a053f48719d4a5bc7f41043a9b2ac7f6ff0a890
Annge001 <anngela0222@gmail.com>
2022-10-26 18:54:15 -0300

    up

---
+++ src/app/modulos/inscripciones/components/form-inscripcion/form-inscripcion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-inscripcion',
+  templateUrl: './form-inscripcion.component.html',
+  styleUrls: ['./form-inscripcion.component.css']
+})
+export class FormInscripcionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
