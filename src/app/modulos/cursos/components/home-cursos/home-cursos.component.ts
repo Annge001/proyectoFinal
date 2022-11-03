4113a1561bb7379b6baf7d0770c8c5faddb6127f
Annge001 <anngela0222@gmail.com>
2022-11-03 08:51:36 -0300

    usé lazy loading en modulo de alumnos y modulo de cursos, revisar bien declaration, imports y exports

---
+++ src/app/modulos/cursos/components/home-cursos/home-cursos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-cursos',
+  templateUrl: './home-cursos.component.html',
+  styleUrls: ['./home-cursos.component.css']
+})
+export class HomeCursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
