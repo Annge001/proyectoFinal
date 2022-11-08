e43a8bb93cc4d9be1f29f714df97b75d14f051d1
Annge001 <anngela0222@gmail.com>
2022-11-08 01:06:19 -0300

    up

--- src/app/modulos/autenticacion/autenticacion.module.ts
+++ src/app/modulos/autenticacion/autenticacion.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { AutenticacionRoutingModule } from './autenticacion-routing.module';
+import {AutenticacionRoutingModule} from './autenticacion-routing.module';
 import {LoginComponent} from "./components/login/login.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {ValidatorLoginService} from "./services/validator-login.service";
 import {SharedModule} from "../../shared/shared.module";
 
 
@@ -23,7 +22,6 @@ import {SharedModule} from "../../shared/shared.module";
     SharedModule
   ],
   providers: [
-    ValidatorLoginService
   ]
 })
 export class AutenticacionModule { }
