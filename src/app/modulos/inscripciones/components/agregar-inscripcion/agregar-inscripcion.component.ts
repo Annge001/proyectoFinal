388868eaba3d70ec55589e1ab49c480710d44662
Annge001 <anngela0222@gmail.com>
2022-11-08 01:05:39 -0300

    up

--- src/app/modulos/inscripciones/components/agregar-inscripcion/agregar-inscripcion.component.ts
+++ src/app/modulos/inscripciones/components/agregar-inscripcion/agregar-inscripcion.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit} from '@angular/core';
 import {InscripcionesService} from "../../services/inscripciones.service";
 import {Inscripcion} from "../../../../models/inscripcion";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-agregar-inscripcion',
@@ -9,18 +11,53 @@ import {Inscripcion} from "../../../../models/inscripcion";
 })
 export class AgregarInscripcionComponent implements OnInit {
 
+  formulario: FormGroup;
   inscripcion: Array<Inscripcion> = []
+  nuevaInscripcion = new EventEmitter<any>();
+
 
   constructor(
-    private inscripcionService: InscripcionesService
+    private fb: FormBuilder,
+    private inscripcionService: InscripcionesService,
+    private router: Router
   ) {
 
+    this.formulario = fb.group({
+      idInscripcion: ['', [Validators.required]],
+      idCurso: ['', [Validators.required]],
+      idAlumno: ['', [Validators.required]],
+      profesor: ['', [Validators.required]],
+      fechaInicio: ['', [Validators.required]],
+      fechaFin: ['', [Validators.required]]
+
+    });
+
+    this.obtenerInscripcion().subscribe(data => {
+      this.inscripcion = data
+      console.log(this.inscripcion)
+
+    })
   }
+
 
   ngOnInit(): void {
   }
 
-  obtenerInscripcion(){
-
+  crearInscripcion() {
+    this.nuevaInscripcion.emit(
+      {
+        idInscripcion: this.formulario.get('idInscripcion')?.value,
+        idCurso: this.formulario.get('idCurso')?.value,
+        idAlumno: this.formulario.get('idAlumno')?.value,
+        profesor: this.formulario.get('profesor')?.value,
+        fechaInicio: this.formulario.get('fechaInicio')?.value,
+        fechaFin: this.formulario.get('fechaFin')?.value,
+      }
+    )
+    this.formulario.reset()
   }
+  obtenerInscripcion() {
+    return this.inscripcionService.obtenerInscripcion();
+  }
+
 }
