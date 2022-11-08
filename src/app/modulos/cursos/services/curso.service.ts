e8f9315460509a2d3386491850533551b2c69f65
Annge001 <anngela0222@gmail.com>
2022-11-08 01:06:49 -0300

    up

--- src/app/modulos/cursos/services/curso.service.ts
+++ src/app/modulos/cursos/services/curso.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Curso} from "../../../models/curso";
 import {BehaviorSubject, Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../environments/environment";
 
 @Injectable()
 export class CursoService {
@@ -12,7 +13,7 @@ export class CursoService {
   }
 
   obtenerCursosPromise(): Observable<Curso[]>{
-    return this.http.get<Curso[]>('https://63642d227b209ece0f42366f.mockapi.io/cursos')
+    return this.http.get<Curso[]>(`${environment.api}/curso/`)
   }
 
   editarCurso(curso: Curso){
