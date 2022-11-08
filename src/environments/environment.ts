909b0bf9a51b09a7c76abe709c34666ccaef1a53
Annge001 <anngela0222@gmail.com>
2022-11-08 01:07:54 -0300

    up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api: 'https://63642d227b209ece0f42366f.mockapi.io/inscripcion'
 };
 
 /*
