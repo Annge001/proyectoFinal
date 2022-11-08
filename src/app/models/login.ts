9831104cdad4acae54969b4805a6652386596a53
Annge001 <anngela0222@gmail.com>
2022-11-08 01:10:07 -0300

    up

--- src/app/models/login.ts
+++ src/app/models/login.ts
@@ -1,7 +1,7 @@
 import { Usuario } from "./usuario";
 
 export interface Sesion {
-  loginActivo: boolean;
+  sesionActiva: boolean;
   //? = para avisar que es opcional, ya que puede ser activo o no.
   usuarioActivo?: Usuario;
 }
