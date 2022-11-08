e7dff9bf6b46b0bb160d7518d3d1f81e8991dacb
Annge001 <anngela0222@gmail.com>
2022-11-08 01:07:09 -0300

    up

--- src/app/modulos/cursos/cursos-routing.module.ts
+++ src/app/modulos/cursos/cursos-routing.module.ts
@@ -4,15 +4,18 @@ import {ListaCursosComponent} from "./components/lista-cursos/lista-cursos.compo
 import {AgregarCursoComponent} from "./components/agregar-curso/agregar-curso.component";
 import {EditarCursoComponent} from "./components/editar-curso/editar-curso.component";
 import {VerDetalleComponent} from "./components/ver-detalle/ver-detalle.component";
+import {CursosComponent} from "./components/cursos/cursos.component";
+import {AutenticacionGuard} from "../../core/guards/autenticacion.guard";
 
 
 //activar canActivate, despues de la segunda entrega.
 const routes: Routes = [
       { path: '',  children:[
-        {path: 'lista-curso',  component: ListaCursosComponent},
-        {path: 'agregar-curso',  component: AgregarCursoComponent},
-        {path: 'editar-curso',  component: EditarCursoComponent} ,
-        {path: 'detalle-curso',  component: VerDetalleComponent}
+        {path: 'lista-curso',  component: ListaCursosComponent ,canActivate: [AutenticacionGuard]},
+        {path: 'agregar-curso',  component: AgregarCursoComponent,canActivate: [AutenticacionGuard]},
+        {path: 'editar-curso',  component: EditarCursoComponent,canActivate: [AutenticacionGuard]} ,
+        {path: 'detalle-curso',  component: VerDetalleComponent,canActivate: [AutenticacionGuard]},
+          {path: 'cursos',  component: CursosComponent,canActivate: [AutenticacionGuard]}
     ]},
 ];
 
