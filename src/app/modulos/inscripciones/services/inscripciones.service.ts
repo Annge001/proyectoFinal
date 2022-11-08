e16363e376d4cc0c5dc02fd7d791f784c2f11e16
Annge001 <anngela0222@gmail.com>
2022-11-08 01:08:18 -0300

    up

--- src/app/modulos/inscripciones/services/inscripciones.service.ts
+++ src/app/modulos/inscripciones/services/inscripciones.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Inscripcion} from "../../../models/inscripcion";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {Observable, BehaviorSubject, catchError, filter, map, Subject, throwError} from "rxjs";
+import {environment} from "../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class InscripcionesService {
 
+  //esta declarado en eviroment.ts! asi se puede usar en los servicios que se necesite
+  //api: string = 'https://63642d227b209ece0f42366f.mockapi.io/inscripcion'
 
   inscripcion: Array<Inscripcion> = []
 
@@ -18,20 +21,53 @@ export class InscripcionesService {
 
   }
 
-  obtenerDetalleInscripcion(): Observable<Inscripcion[]>{
-    return this.http.get<Inscripcion[]>('https://63642d227b209ece0f42366f.mockapi.io/inscripcion')
-
+  obtenerInscripcion(): Observable<Inscripcion[]>{
+    return this.http.get<Inscripcion[]>(`${environment.api}/inscripcion`, {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'encoding': 'UTF-8'
+      })
+    }).pipe(
+    )
   }
 
-  editarInscripcion(inscripcion: Inscripcion){
-    this.http.put(`https://63642d227b209ece0f42366f.mockapi.io/inscripcion${inscripcion.idInscripcion}`, inscripcion)
+  obtenerDetalleInscripcion(): Observable<Inscripcion[]> {
+    return this.http.get<Inscripcion[]>(`${environment.api}/inscripcion`, {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'encoding': 'UTF-8'
+      })
+    })//.pipe(
+      //catchError(this.manejarError)
+    //)
   }
- //borrarInscripcion(idInscipcion:String): Observable<number>{
 
- //}
+  editarInscripcion(inscripcion: Inscripcion) {
+    this.http.put(`${environment.api}/inscripcion${inscripcion.idInscripcion}`, inscripcion, {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'encoding': 'UTF-8'
+      })
+    })//.pipe(
+     // catchError(this.manejarError)
+    //)
+  }
 
-  agregarIncripcion(){
+  borrarInscripcion(id: number) {
+    // @ts-ignore
+    this.http.delete<Inscripcion>(`${environment.api}/inscripcion/${id}/inscripcion`)
+    (catchError(this.obtenerInscripcion)
+   ).subscribe(console.log);
+    alert("Registro eliminado");
+  }
 
+  agregarIncripcion(inscripcion: Inscripcion) {
+    this.http.post(`${environment.api}/inscripcion/`,inscripcion,{
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'encoding': 'UTF-8'
+      })
+    })
   }
 
 
