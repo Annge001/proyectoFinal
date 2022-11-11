e19e9adfb9ae88a00fda76872156f7a87b8f37c2
Annge001 <anngela0222@gmail.com>
2022-11-11 19:32:51 -0300

    up

--- src/app/modulos/usuarios/usuarios-routing.module.ts
+++ src/app/modulos/usuarios/usuarios-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListaUsuariosComponent} from "./components/lista-usuarios/lista-usuarios.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListaUsuariosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
